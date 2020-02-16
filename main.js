9dc171def6209965f2a5a61d5af5cb5e82f1da77
Oleksandr Lopata <djelvz@gmail.com>
2020-02-16 16:52:50 +0200

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,22 @@ let group = [];
 for (let i = 0; i < plays.length; i++) {
   plays[i].addEventListener('click', e => {
     group.push(plays[i]);
+    if (plays[i].className === plays[i + 1].className) {
+      group.push(plays[i + 1]);
+    }
+    if (plays[i].className === plays[i - 1].className) {
+      group.push(plays[i - 1]);
+    }
+    if (plays[i].className === plays[i + 6].className) {
+      group.push(plays[i + 6]);
+    }
+    if (plays[i].className === plays[i - 6].className) {
+      group.push(plays[i - 6]);
+    }
     e.preventDefault();
+    group.forEach(el => {
+      el.innerHTML = '';
+    });
     console.log(group);
   });
 }
